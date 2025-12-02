6ed43d6ad2606e54de2b150413db1d35aa718198
Diego Lima <diegolima20100@gmail.com>
2025-12-02 17:03:01 -0300

    feat: adicionar action customizada para busca de produtos por nome
    - Criar action search_by_name no ProductViewSet
    
    - Implementar busca case-insensitive usando filter name__icontains
    
    - Remover classe redundante StandardResultsSetPagination
    
    - Usar ProductPagination diretamente nos ViewSets
    
    - Endpoint: GET /products/search/?name=termo

--- store_api/api/viewsets.py
+++ store_api/api/viewsets.py
@@ -1,19 +1,34 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .models import Category, Product
 from .serializers import CategorySerializer, ProductSerializer
 from .pagination import ProductPagination
 
-class StandardResultsSetPagination(ProductPagination):
-    page_size = 10
-    page_size_query_param = 'page_size'
-    max_page_size = 100
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    pagination_class = StandardResultsSetPagination
+    pagination_class = ProductPagination
 
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    pagination_class = ProductPagination
+    
+    # Busca produtos pelo nome -> products/search_by_name/?name=notebook
+    @action(detail=False, methods=['get'], url_path='search')
+    def search_by_name(self, request):
+        name = request.query_params.get('name', '')
+        products = self.queryset.filter(name__icontains=name)
+        
+        page = self.paginate_queryset(products)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+        
+        serializer = self.get_serializer(products, many=True)
+        return Response(serializer.data)
+
+    
