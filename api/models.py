0bfb87f4766213e1b74667f6f5a1527ce93d64d1
Marllon Lima <coder.marllon@gmail.com>
2025-12-03 19:33:43 -0300

    #2 criando as class pedidas na models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+class Category(models.Model):
+  name = models.Charfiel(max_length=255)
+  
+  def __str__(self):
+      return self.name
+  
+  class Meta:
+    verbose_name_plural = "Categories"  
+
+
+class Product(models.Model):
+  name = models.Charfiel(max_length=255)
+  description = models.TextField()
+  price = models.DecimalField(max_digits=10, decimal_places=2)
+  category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='products')
+  image = models.ImageField(upload_to='products/', blank=True, null=True, verbose_name="imagem do Produto")
+  
+  def __str__(self):
+      return self.name
+  
 # Create your models here.
