98b9ee9083ee1fa9bb2dafcc4fcae2ea5b7f13e4
Marllon Lima <coder.marllon@gmail.com>
2025-12-03 20:15:50 -0300

    #4 Criando a serializers

---
+++ api/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Category, Product
+
+
+class CategorySerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Category
+    fields = '__all__'
+    
+class ProductSerializer(serializers.MoodelSerializer):
+  category_name = serializers.Charfield(source='category_name', read_only=True)
+  
+  class Meta:
+    model = Product
+    fields = ['id', 'name', 'description', 'price', 'category', 'category_name']
+    read_only_fields = ['category_name']    
